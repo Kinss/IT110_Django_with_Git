7df7df4eb76411f405446b0142ba742966b2b4b2
Kinsey <kinsey6629@gmail.com>
2015-11-20 14:05:07 -0500

    Added timezone and models import in file.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from django.db import models
+from django.utils import timezone
+
+
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
