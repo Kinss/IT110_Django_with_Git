d3686308c2a6098b06bf68a7a10dc097ca01d151
Kinsey <kinsey6629@gmail.com>
2015-11-23 15:48:55 -0500

    Editted admin.py and made Date info collapse and seperated the Date info and Question text into seperate boxes.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text']
+	fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
 
 admin.site.register(Question, QuestionAdmin)
