1eb2f1982bfea8eb51d960ed1d77987adced770a
Kinsey <kinsey6629@gmail.com>
2015-12-02 15:26:14 -0500

    Shortened text in views file.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,18 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-		'latest_question_list': latest_question_list,
-	})
-	return HttpResponse(output)
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
