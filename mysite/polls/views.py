7145d03f96b29ed889d29b5e771530bf0a2c5a35
Kinsey <kinsey6629@gmail.com>
2015-12-02 15:29:56 -0500

    Added in an Http404 response in the views.py file.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,7 @@
+from django.hettp import Http404
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-
 from .models import Question
 
 
@@ -13,7 +11,11 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-	return HttpResponse("You're looking at question %s." % question_id)
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
 	response = "You're looking at the results of question %s."
