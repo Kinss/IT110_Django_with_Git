d00b6ad2e906c56729e984004e2b9b15434113ab
Kinsey <kinsey6629@gmail.com>
2015-12-02 15:18:19 -0500

    Rewrote the index(request) for views.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Question
 
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([p.question_text for p in latest_question_list])
+	template = loader.get_template('polls/index.html')
+	context = RequestContext(request, {
+		'latest_question_list': latest_question_list,
+	})
 	return HttpResponse(output)
-
-
-def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
 	return HttpResponse("You're looking at question %s." % question_id)
