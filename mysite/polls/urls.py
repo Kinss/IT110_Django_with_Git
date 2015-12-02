f0be2cde7e6294b882151b89de1f1f885e58fdb8
Kinsey <kinsey6629@gmail.com>
2015-12-01 22:20:50 -0500

    Had to fix writing in the wrong fileand what not.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,12 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 
 from . import views
-
-urlpatterns = [
-	url(r'^polls/', include('polls.urls')),
-	url(r'^admin/', include(admin.site.urls)),
-]
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
