696dbc9da8903561909cea2c3a951b03499d58cf
Kinsey <kinsey6629@gmail.com>
2015-12-01 22:11:37 -0500

    Added admin and polls url into url.py file.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import urls
+from django.conf.urls import include, url
+from django.contrib import admin
 
 from . import views
+
+urlpatterns = [
+	url(r'^polls/', include('polls.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+]
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
