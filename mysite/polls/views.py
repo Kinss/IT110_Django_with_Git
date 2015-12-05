b41073b49408daf77de4821f8897e3c1bbeed887
Kinsey <kinsey6629@gmail.com>
2015-12-04 22:12:11 -0500

    Found where the errors for poll page began reverted to that commit. Now commiting the revert (I believe).

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,22 +1,40 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.utils import timezone
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
 
 # Create your views here.
-from .models import Question
+from .models import Choice, Question
 
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_question_list'
+	
+	def get_queryset(self):
+		"""
+		Return the last five published questions (not including those set to be
+		published in the future).
+		"""
+		return Question.objects.filter(
+			pub_date__lte=timezone.now()
+		).order_by('-pub_date')[:5]
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/results.html', {'question': question})
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = 'polls/results.html'
 
 def vote(request, question_id):
 	p = get_object_or_404(Question, pk=question_id)
