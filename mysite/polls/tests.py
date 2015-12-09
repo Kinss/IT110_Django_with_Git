ff326614860fcf63ccd515a1a8e75dc10e200a3c
Kinsey <kinsey6629@gmail.com>
2015-12-09 14:40:59 -0500

    Put a C instead of a c, must remember to only capitalize when necessary.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 from .models import Question
 
 
-Class QuestionMethodTests(TestCase):
+class QuestionMethodTests(TestCase):
 	
 	def test_was_published_recently_with_future_question(self):
 		"""
