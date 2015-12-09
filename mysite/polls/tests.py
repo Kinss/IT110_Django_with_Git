90e302d2a2f267c61a5bf936320461a345d9a0a3
Kinsey <kinsey6629@gmail.com>
2015-12-09 14:38:06 -0500

    Needed to add an s to the end of tests.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 from .models import Question
 
 
-Class QuestionMethodTest(TestCase):
+Class QuestionMethodTests(TestCase):
 	
 	def test_was_published_recently_with_future_question(self):
 		"""
